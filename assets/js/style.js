0bdada917ec9ce7639ed83be15bea399f90e9436
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2019-05-22 02:33:57 -0300

    - Organizado o arquivo de style primeiro importação depois os objetos

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,29 +1,29 @@
 // --------------------------------------------
 // Importações de classes que uso em todo o site
 // --------------------------------------------
+import ScrollMenu from './modules/animaMenu.js';
+import ScrollSuave from './modules/scroll-suave.js';
+import Loading from './modules/loading.js';
+import MaskPhone from './modules/mascara.js';
+import AtivaMenu from './modules/ativaMenu.js';
 
 // Evento de animação ao menu ao dar scroll
-import ScrollMenu from './modules/animaMenu.js';
 const scrollmenu = new ScrollMenu('[data-menu="mainMenu"]');
 scrollmenu.init();
 
-// Evento de scroll mais suave ao clickar 
-// alguma tag a
-import ScrollSuave from './modules/scroll-suave.js';
+// Evento de scroll mais suave ao clickar alguma tag com a
 const scrollsuave = new ScrollSuave('[data-animacao]', );
 scrollsuave.init();
 
 // Evento de carregamento de página
-import Loading from './modules/loading.js';
 const loading = new Loading('.loader', 'body');
 loading.init();
 
 // Mascara de telefone com todos os inputs que
 // possuirem o input com o id de telefone
-import MaskPhone from './modules/mascara.js';
 const objectMask = new MaskPhone('#telefone');
 objectMask.init();
 
-import AtivaMenu from './modules/ativaMenu.js';
+// Evento para ativar animação do menu
 const ativaMenu = new AtivaMenu('[data-menu="button"]', '[data-menu="list"]');
 ativaMenu.init();
